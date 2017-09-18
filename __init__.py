cb1ee1cfaf5704ac846cf3cdd05fffd8ebc5541a
TerCZ <t.chuzhe@qq.com>
2017-09-18 20:48:32 +0800

    test

--- __init__.py
+++ __init__.py
@@ -8,11 +8,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    app.logger.debug(request.form)
-    app.logger.debug(request.args)
-    print(request.form)
-    print(request.args)
-
+    s = request["signature"]
+    s += request["timestamp"]
+    s += request["nonce"]
+    s += request["echostr"]
 
     # signature = data.signature
     # timestamp = data.timestamp
@@ -31,8 +30,7 @@ def hello_world():
     # else:
     #     return ""
 
-    return 'Hello, World!'
+    return s
 
 if __name__ == "__main__":
     app.run()
-
