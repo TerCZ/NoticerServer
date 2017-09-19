925c4f7c8150191d9bff836e6941cc83955b86de
TerCZ <t.chuzhe@qq.com>
2017-09-19 20:29:11 +0800

    make response a repeater

--- __init__.py
+++ __init__.py
@@ -3,21 +3,33 @@
 import logging
 import xml.etree.ElementTree as ET
 
+from . import util
 from flask import Flask, request
-from .util import is_from_wechat, text_reply, default_reply
+
 
 app = Flask(__name__)
+
+
+def save_message(wechat_open_id, message):
+    # todo
+    pass
+
+
+def deal_message(wechat_open_id, message):
+    save_message(wechat_open_id, message)
+
+    return "你的微信OpenID是：「{}」\n\n刚发送了：「{}」".format(wechat_open_id, message)
 
 
 @app.route("/", methods=["GET"])
 def hello_world():
     echostr = request.args.get("echostr", "")
-    return echostr if is_from_wechat(request) else "这不是微信请求呢"
+    return echostr if util.is_from_wechat(request) else "这不是微信请求呢"
 
 
 @app.route("/", methods=["POST"])
 def receive_text():
-    if not is_from_wechat(request):
+    if not util.is_from_wechat(request):
         return "这不是微信请求呢"
 
     # get common data
@@ -31,9 +43,10 @@ def receive_text():
         content = xmldata.find("Content").text
         msg_id = xmldata.find("MsgId").text
 
-        return default_reply(to_user=from_user_name, from_user=to_user_name)
+        reply = deal_message(from_user_name, content)
+        return util.text_reply(to_user=from_user_name, from_user=to_user_name, content=reply)
     else:
-        return default_reply(to_user=from_user_name, from_user=to_user_name)
+        return util.default_reply(to_user=from_user_name, from_user=to_user_name)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -3,7 +3,8 @@ from time import time
 
 
 TOKEN = "LongAsHeLives"
-DEFAULT_PROMPT = "欢迎使用SJTU Noticer校园信息订阅服务！通过微信号您可以管理您的订阅，我们将为您定期通过邮件推送最新校园通知。\n\n您可以发送以下消息进行操作：\n「邮箱 your@email.com」，注册（或更新）您的邮箱，请替换your@email.com为您的邮箱；\n「信息来源」查看当前可用的信息来源；\n「订阅 7」，开启订阅并设置（或更新）推送周期为7天，请替换7为您偏好的推送周期；\n「取消」，取消当前的邮件推送，您的订阅设置不会被删除。\n\n您也可以通过留言进行反馈，帮助我们做得更好！"
+DEFAULT_PROMPT = "欢迎使用SJTU Noticer校园信息订阅服务！通过微信号您可以管理您的订阅，我们将为您定期通过邮件推送最新校园通知。\n\n您可以发送以下消息进行操作：\n【邮箱 your@email.com】，注册（或更新）您的邮箱，请替换your@email.com为您的邮箱；\n【信息来源】查看当前可用的信息来源；\n【订阅 7】，开启订阅并设置（或更新）推送周期为7天，请替换7为您偏好的推送周期；\n【取消】，取消当前的邮件推送，您的订阅设置不会被删除。\n\n您也可以通过留言进行反馈，帮助我们做得更好！"
+
 
 def is_from_wechat(request):
     signature = request.args.get("signature", "")
