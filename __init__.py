f4c707758ca9d40661437c9848ade095265370a2
TerCZ <t.chuzhe@qq.com>
2017-09-19 16:58:29 +0800

    add default text reply

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import logging
 import xml.etree.ElementTree as ET
 
 from flask import Flask, request
-from .util import is_from_wechat
+from .util import is_from_wechat, text_reply, default_reply
 
 app = Flask(__name__)
 
@@ -21,24 +21,20 @@ def receive_text():
         return "这不是微信请求呢"
 
     # get common data
-    message = request.data.decode("ascii")
-    xmldata = ET.fromstring(message)
+    xmldata = ET.fromstring(request.data.decode("utf8"))
     from_user_name = xmldata.find("FromUserName").text
     to_user_name = xmldata.find("ToUserName").text
     create_time = xmldata.find("CreateTime").text
     msg_type = xmldata.find("MsgType").text
 
-    logging.debug("ToUserName: {}\nFromUserName: {}\nCreateTime: {}\nMsgType: {}\n".format(to_user_name, from_user_name, create_time))
-
-
     if msg_type == "text":
-        content = xmldata.find("Content").text
+        content_encoded = xmldata.find("Content").text
+        content = bytes.fromhex(content_encoded.replace("\\x", "")).decode('utf-8')  # stupid wechat
         msg_id = xmldata.find("MsgId").text
 
-        logging.debug("Content: {}\nMsgId: {}\n".format(to_user_name, from_user_name, create_time, content, msg_id))
-
-
-    return "success"
+        return default_reply(to_user=from_user_name, from_user=to_user_name)
+    else:
+        return default_reply(to_user=from_user_name, from_user=to_user_name)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 import hashlib
+from time import time
 
 
 TOKEN = "LongAsHeLives"
-
+DEFAULT_PROMPT = "欢迎使用SJTU Noticer校园信息订阅服务！通过微信号您可以管理您的订阅，我们将为您定期通过邮件推送最新校园通知。\n\n您可以发送以下消息进行操作：\n「邮箱 your@email.com」，注册（或更新）您的邮箱，请替换your@email.com为您的邮箱；\n「信息来源」查看当前可用的信息来源；\n「订阅 7」，开启订阅并设置（或更新）推送周期为7天，请替换7为您偏好的推送周期；\n「取消」，取消当前的邮件推送，您的订阅设置不会被删除。\n\n您也可以通过留言进行反馈，帮助我们做得更好！"
 
 def is_from_wechat(request):
     signature = request.args.get("signature", "")
@@ -19,3 +20,13 @@ def is_from_wechat(request):
         return True
     else:
         return False
+
+
+def text_reply(to_user, from_user, content):
+    template = "<xml><ToUserName><![CDATA[{to_user}]]></ToUserName><FromUserName><![CDATA[{from_user}]]></FromUserName><CreateTime>{create_time}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{content}]]></Content></xml>"
+    return template.format(to_user=to_user, from_user=from_user, create_time=int(time()), content=content)
+
+
+def default_reply(to_user, from_user):
+    template = "<xml><ToUserName><![CDATA[{to_user}]]></ToUserName><FromUserName><![CDATA[{from_user}]]></FromUserName><CreateTime>{create_time}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{content}]]></Content></xml>"
+    return template.format(to_user=to_user, from_user=from_user, create_time=int(time()), content=DEFAULT_PROMPT)
