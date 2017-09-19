46e8a5b875d37846505d2c533b882b9be38278dc
TerCZ <t.chuzhe@qq.com>
2017-09-19 16:08:50 +0800

    try to parse xml

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: <encoding name> -*-
 import logging
+import xml.etree.ElementTree as ET
 
 from flask import Flask, request
 from .util import is_from_wechat
@@ -19,19 +20,23 @@ def receive_text():
     if not is_from_wechat(request):
         return "这不是微信请求呢"
 
-    message = request.data.decode("utf8")
-    logging.debug(message)
+    # get common data
+    message = request.data.decode("ascii")
+    xmldata = ET.fromstring(message)
+    from_user_name = xmldata.find("FromUserName").text
+    to_user_name = xmldata.find("ToUserName").text
+    create_time = xmldata.find("CreateTime").text
+    msg_type = xmldata.find("MsgType").text
 
-    # to_user_name = request.form.get("ToUserName")
-    # from_user_name = request.form.get("FromUserName")
-    # create_time = request.form.get("CreateTime")
-    # msg_type = request.form.get("MsgType")
-    # if msg_type == "text":
-    #     content = request.form.get("Content")
-    #     msg_id = request.form.get("MsgId")
-    #
-    #     app.logger.debug("ToUserName: {}\tFromUserName: {}\tCreateTime: {}\tMsgType: {}\tContent: {}\tMsgId: {}\t".format(
-    #         to_user_name, from_user_name, create_time, content, msg_id))
+    logging.debug("ToUserName: {}\nFromUserName: {}\nCreateTime: {}\nMsgType: {}\n".format(to_user_name, from_user_name, create_time))
+
+
+    if msg_type == "text":
+        content = xmldata.find("Content").text
+        msg_id = xmldata.find("MsgId").text
+
+        logging.debug("Content: {}\nMsgId: {}\n".format(to_user_name, from_user_name, create_time, content, msg_id))
+
 
     return "success"
 
