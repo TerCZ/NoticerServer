3efcaf05f46268b2d38235bb62202dfd6ca07008
TerCZ <t.chuzhe@qq.com>
2017-09-23 17:25:14 +0800

    actually deal with message

--- __init__.py
+++ __init__.py
@@ -33,7 +33,69 @@ def save_message(wechat_open_id, message):
 def deal_message(wechat_open_id, message):
     save_message(wechat_open_id, message)
 
-    return "你的微信OpenID是：「{}」\n\n刚发送了：「{}」".format(wechat_open_id, message)
+    cursor = CONN.cursor()
+    reply = "你的微信OpenID是：「{}」\n\n刚发送了：「{}」".format(wechat_open_id, message)
+    if message.startswith("取消"):
+        try:
+            sql = "SELECT COUNT(*) FROM USER WHERE wechat_open_id = %s"
+            cursor.execute(sql, (wechat_open_id,))
+
+            if cursor.fetchone()[0] == 0:  # not register email yet
+                reply = "您尚未注册邮箱，发送「邮箱 email_address」进行注册！")
+            else:  # update user
+                sql = "UPDATE User SET activated = FALSE WHERE wechat_open_id = %s"
+                cursor.execute(sql, (wechat_open_id,))
+                reply = "取消推送成功！"
+        except Exception as e:
+            logging.error(e)
+    else if message.startswith("订阅"):
+        try:
+            _, interval = message.split()
+            sql = "SELECT COUNT(*) FROM USER WHERE wechat_open_id = %s"
+            cursor.execute(sql, (wechat_open_id,))
+
+            if cursor.fetchone()[0] == 0:  # not register email yet
+                reply = "您尚未注册邮箱，发送「邮箱 email_address」进行注册！")
+            else:  # update user
+                sql = "UPDATE User SET sending_interval = %s WHERE wechat_open_id = %s"
+                cursor.execute(sql, (interval, wechat_open_id))
+                reply = "更新推送周期为{}天！".format(interval)
+            sql = "UPDATE User SET activated = TRUE, sending_interval = %s WHERE wechat_open_id = %s"
+            cursor.execute(sql, (interval, wechat_open_id))
+        except Exception as e:
+            logging.error(e)
+    else if message.startswith("信息来源"):
+        try:
+            sql = "SELECT school_name, school_id FROM School"
+            cursor.execute(sql)
+
+            reply = "SJTU Noticer目前提供以下信息分类：\n"
+            for entry in cursor.fetchall()
+                name, school_id = entry
+                reply += "\t{}，{}\n".format(name, school_id)
+            reply += "\n发送「详情 X」查看该类别提供的具体项目，X为类别后面的数字"
+        except Exception as e:
+            logging.error(e)
+    else if message.startswith("邮箱"):
+        try:
+            _, email = message.split()
+            sql = "SELECT COUNT(*) FROM USER WHERE wechat_open_id = %s"
+            cursor.execute(sql, (wechat_open_id,))
+
+            if cursor.fetchone()[0] == 0:  # create user
+                sql = "INSERT INTO User (wechat_open_id, email) VALUES (%s, %s)"
+                cursor.execute(sql, (wechat_open_id, email))
+                reply = "注册邮箱{}成功！\n默认推送周期为7天，您可通过发送「订阅 X」更新为X天。\n您可通过发送「信息来源」查看并订阅校园信息。".format(email)
+            else:  # update user
+                sql = "UPDATE User SET email = %s WHERE wechat_open_id = %s"
+                cursor.execute(sql, (email, wechat_open_id))
+                reply = "更新邮箱为{}！".format(email)
+        except Exception as e:
+            logging.error(e)
+
+    CONN.commit()
+
+    return reply
 
 
 @app.route("/", methods=["GET"])
